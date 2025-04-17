40ed886d7e2a1a2fca3b44982aa17609df6604ad
MangoCode <arhamsiddiqui344@gmail.com>
2025-04-17 16:54:23 -0400

    Website Update

--- website/js/main.js
+++ website/js/main.js
@@ -34,31 +34,6 @@ $(document) .ready(function() {
     }
    });
 
-  
-   // skills chart
-  $(document).ready(function(e) {
-  //var windowBottom = $(window).height();
-  var index=0;
-  $(document).scroll(function(){
-    var top = $('#skills').height()-$(window).scrollTop();
-    
-    if(top<-600){
-      if(index==0){ 
-      
-        $('.chart').easyPieChart({
-          easing: 'easeOutBounce',
-          onStep: function(from, to, percent) {
-            $(this.el).find('.percent').text(Math.round(percent));
-          }
-        });
-      
-        }
-      index++;
-    }
-  })
-   
-  });
-
   /*Swiper*/
    var swiper = new Swiper('.swiper', {
      autoplay: {
